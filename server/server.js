745efc12b7da0eaee4c0ee2579edebe2a140abb7
Denver <cudedenver@gmail.com>
2025-08-07 13:05:11 -0700

    chore(app): initialize app with basic scaffolding of server, db, and client

---
+++ server/server.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGODB_URI)
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(error => console.error('MongoDB connection error:', error));
+
+mongoose.connection.on('error', error => {
+    console.error('MongoDB connection error:', error);
+})
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Counter API is running!' });
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
